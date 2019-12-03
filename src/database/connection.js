5cf9b048d90c883fc505d9e4b48870d05b2c24a2
learning-dev <ayazm.140895@gmail.com>
2019-12-03 20:05:45 +0530

    add database connection file and movies.json

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,22 @@
 
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("movies-database", "ayaz", "password", {
+const sequelize = new Sequelize("moviesdatabase", "ayaz", "password", {
   host: "localhost",
-  dialect: "msql",
-  operatorAliases: false,
+  dialect: "mysql",
+ // operatorAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  define: {
+    timestamps: false
+}  
+
 });
+
 
 module.exports = sequelize;
 global.sequelize = sequelize;
