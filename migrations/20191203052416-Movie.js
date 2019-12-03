21a85ff8be6b7958ca19356cdedbfb09212dd370
learning-dev <ayazm.140895@gmail.com>
2019-12-03 11:59:35 +0530

    add drop table interface for movie tabl

--- migrations/20191203052416-Movie.js
+++ migrations/20191203052416-Movie.js
@@ -46,12 +46,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable("Movie");
   }
 };
