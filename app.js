14fce555629a0b6a125fba5cfdacbde3954500df
learning-dev <ayazm.140895@gmail.com>
2019-12-03 21:30:54 +0530

    add tester app.js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-require('./database/connection.js');
+db = require('./src/database/connection');
+
+db.authenticate()
+  .then(() => console.log('connected to Db!'))
+  .catch((err) => {
+    console.log(err);
+  });
+const build = require('./src/database/populateDb');
+build();
+
 
 
 
