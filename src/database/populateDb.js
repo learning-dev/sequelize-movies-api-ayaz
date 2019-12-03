b536b3636aa82daa07a23b4ce11470a7f0881d0b
learning-dev <ayazm.140895@gmail.com>
2019-12-03 21:32:17 +0530

    add temporary seeder file

---
+++ src/database/populateDb.js
@@ -0,0 +1,67 @@
+'use strict'
+module.exports =  async () => {
+
+  const movieData = require('./movies.json');
+  const Director = require('../models/Director');
+  
+  db = require('./connection');
+  db.authenticate().then( ()=> console.log('x'));
+  const Movie = db.import('../models/movie.js');
+  console.log(Movie);
+  console.log(Director);
+  const listOfDirector = [];
+  const directorObjectsToInsert = []
+  const movieObjectsToInsert = [];
+
+  movieData.forEach((movie) => {
+    if (listOfDirector.indexOf(movie.Director) === -1) {
+      listOfDirector.push(movie.Director);
+    }
+  });
+
+  let count = 1;
+  listOfDirector.forEach((element) => {
+    let rowObject = {}
+    rowObject['id'] = count;
+    rowObject['director_name'] = element;
+    count += 1;
+    directorObjectsToInsert.push(rowObject);
+  });
+
+  movieData.forEach((movie) => {
+    let singleMovie = {};
+    let fieldCount = 0;
+    let databaseFields = ['id', 'title', 'description', 'runtime', 'genre', 'rating', 'metascore', 
+    'votes', 'gross_earning_mil', 'director_id', 'actor', 'year'];
+    const fieldsMovie = Object.keys(movie);
+    fieldsMovie.forEach((field) => {
+      if (field === 'Director') {
+        const directorId = listOfDirector.indexOf(movie[field]) + 1;
+        singleMovie['director_id'] = directorId;
+      } else {
+        singleMovie[databaseFields[fieldCount]] = movie[field];
+      }
+      fieldCount += 1;
+    });
+    movieObjectsToInsert.push(singleMovie);
+  });
+  // await Director.bulkCreate(directorObjectsToInsert)
+  //               .then((entry) => {console.log(entry)})
+  //               .catch((err) => console.log(err));
+  await Movie.create({
+    id:1,
+    title: 'xyz', 
+    description:'xdz',
+    runtime: 151,
+    genre:'Crime',
+    rating:8.5,
+    metascore:999, 
+    votes:23,
+    gross_earning_mil:'21312.2',
+    director_id:1, 
+    actor:'sdfs',
+    year:'1932',
+  }).then((entry) => {console.log(entry)})
+    .catch((err) => console.log(err));
+
+}
