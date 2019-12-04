52f49d5f522f9f9b7ca310eb20e92f64e0cf39e2
learning-dev <ayazm.140895@gmail.com>
2019-12-04 12:56:37 +0530

    format code according to airbnb style

--- migrations/20191203125009-create_movie_table.js
+++ migrations/20191203125009-create_movie_table.js
@@ -1,57 +1,55 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("Movies", {
-      id: {
-        type: Sequelize.INTEGER(11),
-        allowNull: false,
-        autoIncrement: true, 
-        primaryKey: true
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Movies', {
+    id: {
+      type: Sequelize.INTEGER(11),
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING(100),
+    },
+    runtime: {
+      type: Sequelize.INTEGER(10),
+    },
+    genre: {
+      type: Sequelize.STRING(100),
+    },
+    rating: {
+      type: Sequelize.FLOAT(3),
+    },
+    metascore: {
+      type: Sequelize.STRING(10),
+    },
+    votes: {
+      type: Sequelize.INTEGER(11),
+    },
+    gross_earning_mil: {
+      type: Sequelize.STRING(20),
+    },
+    director_id: {
+      type: Sequelize.INTEGER(11),
+      allowNull: false,
+      references: {
+        model: 'Directors',
+        key: 'id',
       },
-      title: {
-        type: Sequelize.STRING(100),
-        allowNull: false
-      },
-      description: {
-        type: Sequelize.STRING(100),
-      },
-      runtime: {
-        type: Sequelize.INTEGER(10),
-      },
-      genre: {
-        type: Sequelize.STRING(100),
-      },
-      rating:{
-        type: Sequelize.FLOAT(3),
-      },
-      metascore: {
-        type: Sequelize.STRING(10),
-      },
-      votes: {
-        type: Sequelize.INTEGER(11),
-      },
-      gross_earning_mil: {
-        type: Sequelize.STRING(20),
-      },
-      director_id: {
-        type: Sequelize.INTEGER(11),
-        allowNull: false,
-        references: {
-          model: 'Directors',
-          key: 'id'  
-        }
-      },
-      actor: {
-        type: Sequelize.STRING(200),
-      },
-      year: {
-        type: Sequelize.INTEGER(4)
-      }
-    });
-  },
+    },
+    actor: {
+      type: Sequelize.STRING(200),
+    },
+    year: {
+      type: Sequelize.INTEGER(4),
+    },
+  }),
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("Movies");
-  }
+    return queryInterface.dropTable('Movies');
+  },
 };
--- migrations/20191204044352-modify_movie_table_descrption_column.js
+++ migrations/20191204044352-modify_movie_table_descrption_column.js
@@ -3,12 +3,12 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.changeColumn('Movies', 'description', {
     type: Sequelize.STRING(255),
-  })
+  });
 }
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.changeColumn('Movies', 'description', {
     type: Sequelize.STRING(100),
-  })
+  });
 }
 };
