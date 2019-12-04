52d7214b4181f626408d473736649dae848f5809
learning-dev <ayazm.140895@gmail.com>
2019-12-04 15:31:08 +0530

    add getall() and getByID for movie and director models in routes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const Movie = db.import('./src/models/movie');
+const Director = require('./src/models/Director');
 
 console.log(Movie);
 
@@ -26,6 +27,49 @@ app.get('/api/movies', (req, res) => {
     }
   }).catch((err) => res.status(404).send({ data: { error: "Internal error! can't retrieve movies." } }));
 });
+
+app.get('/api/movies/:id', (req, res) => {
+  const givenId = req.params.id;
+  const promiseObject = Movie.findOne({ where: { id: givenId } });
+  promiseObject.then((result) => {
+    console.log(result);
+    console.log(Object.keys(result));
+    res.status(200).send(result);
+  }).catch((error) => {
+    console.log(error);
+    const errorMessage = { data: { errorMessage: `Resource with id ${req.params.id} doesn't exist` } };
+    res.status(404).send(errorMessage);
+  });
+});
+
+
+app.get('/api/directors', (req, res) => {
+  const promiseObject = Director.findAll();
+  promiseObject.then((result) => {
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      throw (new Error('No data sent from table.'));
+    }
+  }).catch((err) => {
+    console.log(err);
+    res.status(404).send({ data: { error: "Internal error! can't retrieve Directors." } });
+  });
+});
+
+app.get('/api/directors/:id', (req, res) => {
+  const givenId = req.params.id;
+  const promiseObject = Director.findOne({ where: { id: givenId } });
+  promiseObject.then((result) => {
+    console.log(result);
+    res.status(200).send(result);
+  }).catch((error) => {
+    console.log(error);
+    const errorMessage = { data: { errorMessage: `Resource with id ${req.params.id} doesn't exist` } };
+    res.status(404).send(errorMessage);
+  });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
