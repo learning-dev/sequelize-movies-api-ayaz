d0c93d9bcc44f62fee3080a67ae8be1e472d9e05
learning-dev <ayazm.140895@gmail.com>
2019-12-04 13:30:48 +0530

    add get movies method using sequilze method

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const db = require('./src/database/connection');
-const build = require('./src/database/populateDb');
 
 db.authenticate()
   .then(() => console.log('connected to Db!'))
@@ -7,4 +9,25 @@ db.authenticate()
     console.log(err);
   });
 
-build();
+const app = express();
+app.use(bodyParser.json());
+
+const Movie = db.import('./src/models/movie');
+
+console.log(Movie);
+
+app.get('/api/movies', (req, res) => {
+  const promiseObject = Movie.findAll();
+  promiseObject.then((result) => {
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      throw (new Error('No data sent from table.'));
+    }
+  }).catch((err) => res.status(404).send({ data: { error: "Internal error! can't retrieve movies." } }));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Movie and Director RESTful API server started on ${port}`);
+});
